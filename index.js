c240d93509d2790ae55a7b8d98c8b95e62d3214f
Vyomrana02 <87069619+Vyomrana02@users.noreply.github.com>
2022-06-02 19:07:41 +0530

    api change

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCHAPI =
     "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
+
 const main = document.getElementById("main");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
